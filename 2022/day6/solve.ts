9f087f9c6c1b1060cfdd478b337c0ab6423045cf
Steven Indzeoski <steven.indzeoski@gmail.com>
2022-12-07 18:14:40 -0600

    Finishing day 6.

---
+++ 2022/day6/solve.ts
@@ -0,0 +1,54 @@
+import { open } from '../shared';
+
+function findMarker(buffer: string, size: number): number {
+  let start = 0;
+  let end = size;
+
+  while (end < buffer.length) {
+    const chars = new Set(buffer.substring(start, end));
+
+    if (chars.size === size) {
+      return end;
+    }
+
+    start += 1;
+    end += 1;
+  }
+
+  throw Error('No marker found');
+}
+
+function findMessageMarker(buffer: string): number {
+  return findMarker(buffer, 14);
+}
+
+function findPacketMarker(buffer: string): number {
+  return findMarker(buffer, 4);
+}
+
+async function main(inputFile: string) {
+  await part1(inputFile);
+  await part2(inputFile);
+}
+
+async function parseLine(inputFile: string): Promise<string> {
+  for await (const line of open(inputFile)) {
+    return line;
+  }
+
+  return '';
+}
+
+async function part1(inputFile: string) {
+  const buffer = await parseLine(inputFile);
+  const n_processed = findPacketMarker(buffer);
+  console.log(`Start of packet marker after ${n_processed} characters`);
+}
+
+async function part2(inputFile: string) {
+  const buffer = await parseLine(inputFile);
+  const n_processed = findMessageMarker(buffer);
+  console.log(`Start of message marker after ${n_processed} characters`);
+}
+
+main('input.txt');
